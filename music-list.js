50775a9d1772a8afe2da1210d872372e6ad75453
Ayush Tyagi <ayush.tyagi145@gmail.com>
2022-02-26 18:07:45 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+  {
+    name: "Close Eyes",
+    artist: "DVRST",
+    img: "close eyes",
+    src: "close eyes",
+  },
+  {
+    name: "Kostromin",
+    artist: "Gleb Kostromin",
+    img: "kostromin",
+    src: "kostromin",
+  },
+  {
+    name: "Mekanin Sahibi",
+    artist: "Norm Ender",
+    img: "mekanin sahibi",
+    src: "mekanin sahibi",
+  },
+  {
+    name: "One Right Now",
+    artist: "The Weeknd, Post Malone",
+    img: "one right now",
+    src: "one right now",
+  },
+  {
+    name: "Uptown Funk",
+    artist: "Mark Ronson",
+    img: "uptown funk",
+    src: "Uptown funk",
+  },
+];
